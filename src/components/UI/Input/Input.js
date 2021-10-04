e5bf096a15c3d5783ceb6690d09590f6dafec069
Victor Shevchuk <v.shevchuk@viseven.com>
2021-10-04 10:22:21 +0400

    added useImperativeHandle and forwardRef

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useRef, useImperativeHandle } from 'react';
 
 import classes from './Input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const focus = () => inputRef.current.focus();
+
+  useImperativeHandle(ref, () => {
+    return { focus: focus };
+  });
+
   return (
     <div
       className={`${classes.control} ${
@@ -11,6 +19,7 @@ const Input = (props) => {
     >
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        ref={inputRef}
         type={props.type}
         id={props.id}
         value={props.value}
@@ -19,6 +28,6 @@ const Input = (props) => {
       />
     </div>
   );
-};
+});
 
 export default Input;
